6271d331d78ecc28a16a183a0f126d609caa0ee0
joshbp <joshbp@brandeis.edu>
2022-12-13 17:58:42 -0500

    Loading all Brown data

--- src/main.py
+++ src/main.py
@@ -1,9 +1,42 @@
+import os
+import pickle
 import vectors
 import utils
+import rank
+
+from nltk.corpus import brown
+from rank import Document
+
+PREPROCESS_PATH: str = "preprocessed.pkl"
+
+
+def prepare_documents(path: str) -> list[Document]:
+    if os.path.exists(path):
+        print(f"Found preprocessed file {path}!")
+        with open(path, 'rb') as f:
+            return pickle.load(f)
+
+    print("Preprocessing...")
+    documents = []
+    for name in brown.fileids():
+        print(name)
+        sentences = utils.load_file(name)
+        documents.append(Document(name, utils.preprocess(sentences)))
+
+    # save documents
+    with open(path, 'wb') as f:
+        pickle.dump(documents, f)
+
+    return documents
 
 
 def main():
-    pass
+    print("Downloading data...")
+    utils.download()
+    print("Preparing documents...")
+    documents = prepare_documents(PREPROCESS_PATH)
+    print("Loading GloVe embeddings...")
+    glove = vectors.load_glove()
 
 
 if __name__ == "__main__":
