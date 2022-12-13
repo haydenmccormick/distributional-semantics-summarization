1eea8877a2d993c0eed7f3aa3b1c60c7a5acf468
joshbp <joshbp@brandeis.edu>
2022-12-12 23:26:16 -0500

    move document class to rank.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,6 @@
 import vectors
 import utils
 
-from collections import Counter
-
-
-class Document:
-    """Document object that holds information for Brown files."""
-
-    def __init__(self, name: str, sentences: list[str]):
-        self.name = name
-        self.sentences = sentences
-        self.token_counts = self.count_tokens()
-
-    def count_tokens(self) -> Counter[str]:
-        counts = Counter()
-        for sentence in self.sentences:
-            counts.update(sentence)
-        return counts
-
 
 def main():
     pass
--- src/rank.py
+++ src/rank.py
@@ -2,6 +2,21 @@ from math import log
 from collections import Counter
 
 
+class Document:
+    """Document object that holds information for Brown files."""
+
+    def __init__(self, name: str, sentences: list[str]):
+        self.name = name
+        self.sentences = sentences
+        self.token_counts = self.count_tokens()
+
+    def count_tokens(self) -> Counter[str]:
+        counts = Counter()
+        for sentence in self.sentences:
+            counts.update(sentence)
+        return counts
+
+
 def document_frequencies(vocab, documents) -> Counter[str]:
     frequencies = Counter()
     for token in vocab:
