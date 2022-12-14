91fbd645202002c371a5a28197e32104df9dcfc0
taylorsherer <tsherer1@gmail.com>
2022-12-14 16:18:13 -0500

    Unrolling scores summing, adding normalization

--- src/rank.py
+++ src/rank.py
@@ -49,7 +49,8 @@ def tfidf(sentence: list[str],
     term in the sentence."""
     total = 0.
     for term in sentence:
-        tf = log(document.token_counts[term] + 1, 10)
+        # tf = log(document.token_counts[term] + 1, 10)
+        tf = document.token_counts[term]  # / sum(document.token_counts.values())
         idf = log(num_documents / frequencies[term], 10)
         total += tf * idf
     return total
@@ -65,21 +66,22 @@ def count_np_vp(tags: list[str],
     proper_noun_score = sum([sentence_tags[tag] for tag in proper_noun_tags])
     noun_score = sum([sentence_tags[tag] for tag in noun_tags])
     verb_score = sum([sentence_tags[tag] for tag in verb_tags])
-    return (noun_score+verb_score)/total_length, (proper_noun_score)/total_length
+    return (noun_score + verb_score) / total_length, proper_noun_score / total_length
 
 
 def cosine_similarity(vecs: torch.Tensor, index: int):
     if index != 0:
-        tensor1 = vecs[index-1]
+        tensor1 = vecs[index - 1]
         tensor2 = vecs[index]
         cos = torch.nn.CosineSimilarity(dim=0)
         return float(cos(tensor1, tensor2))
     else:
         return 0.0
 
+
 def normalized_sentence_len(sentence, sentences):
     max_len = max([len(sent) for sent in sentences])
-    return len(sentence)/max_len
+    return len(sentence) / max_len
 
 
 # TODO: Normalization and cue phrases
@@ -90,19 +92,48 @@ def rank(sentences: list[list[str]],
     """Generate ranking for each sentence in a list of sentences"""
     scores = np.zeros(len(sentences))
     freqs = document_frequencies(documents)
+
+    s_len_values_normalized = np.zeros(len(sentences))
+    s_pos_values = np.zeros(len(sentences))
+    tfidf_values = np.zeros(len(sentences))
+    np_vp_values_normalized = np.zeros(len(sentences))
+    pn_values_normalized = np.zeros(len(sentences))
+    cosine_similarity_values = np.zeros(len(sentences))
+
     for i, sentence in enumerate(sentences):
         # Sentence length feature
-        s_len = normalized_sentence_len(sentence, sentences)
+        s_len = len(sentence) #normalized_sentence_len(sentence, sentences)
+        s_len_values_normalized[i] = s_len
         # Sentence position feature
-        s_pos = 1-((i-1)/(len(sentences)))
+        s_pos = 1 - (((i+1) - 1) / (len(sentences)))
+        s_pos_values[i] = s_pos
         # TF-IDF feature
         tfidf_freq = tfidf(sentence, document, freqs, len(documents))
+        tfidf_values[i] = tfidf_freq
         # NP, VP, and Proper Noun features
         np_vp_score, proper_noun_score = count_np_vp(document.tags, i)
+        np_vp_values_normalized[i] = np_vp_score
+        pn_values_normalized[i] = proper_noun_score
         # Cosine similarity feature
         cos_similarity = cosine_similarity(vectors, i)
-        scores[i] = sum([s_len, s_pos, tfidf_freq, np_vp_score, proper_noun_score, cos_similarity])
+        cosine_similarity_values[i] = cos_similarity
+
+    s_len_max = max(s_len_values_normalized)
+    tfidf_max = max(tfidf_values)
+    cos_similarity_max = max(cosine_similarity_values)
+
+    for i in range(0, len(sentences)):
+        scores[i] = sum([
+            s_len_values_normalized[i]/s_len_max,
+            s_pos_values[i],
+            tfidf_values[i] / tfidf_max,
+            np_vp_values_normalized[i],
+            pn_values_normalized[i],
+            cosine_similarity_values[i] / cos_similarity_max
+        ])
+
     return scores
+
 
 def summarize(clusters, scores, document, n):
     """Produce summary containing only top n sentences per cluster"""
